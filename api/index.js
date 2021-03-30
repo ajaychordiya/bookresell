537dacdf490d75569a2fea1b1ba52f0673c24a10
ajaychordiya <70837858+ajaychordiya@users.noreply.github.com>
2021-03-30 15:10:48 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 //const bodyParser = require('body-parser');
-const url =
-  "mongodb+srv://user:1234@cluster0.isczk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const url ="your url";
 const path = require("path");
 const port = process.env.PORT || 8080;
 
